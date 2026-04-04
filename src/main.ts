465daf44759574f1d4340a0234ed16288126c443
emanomary <emankullab@gmail.com>
2026-04-04 23:35:18 +0300

    new

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import './style.css';
+import './style.css'
 
 
 // import typescriptLogo from './assets/typescript.svg'
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,25 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite' // أو حسب طريقة استيراد التيلويند لديكِ
 
 // https://vitejs.dev/config/
-export default defineConfig({
+// export default defineConfig({
+//   plugins: [
+//     tailwindcss(),
+//   ],
+//   // تأكدي أن هذا الاسم يطابق تماماً اسم المستودع (Repository) في GitHub
+//   base: '/my-portfolio/', 
+//   build: {
+//     outDir: 'dist', // التأكد من أن المخرجات تذهب لمجلد dist
+//   }
+// })
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
   plugins: [
     tailwindcss(),
   ],
@@ -11,4 +29,4 @@ export default defineConfig({
   build: {
     outDir: 'dist', // التأكد من أن المخرجات تذهب لمجلد dist
   }
-})
+}

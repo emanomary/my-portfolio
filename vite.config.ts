7a009ce730bfab13cfa9489b1a813cd7f49c4925
emanomary <emankullab@gmail.com>
2026-04-06 11:09:38 +0300

    update 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,1 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite' // أو حسب طريقة استيراد التيلويند لديكِ
-
-export default defineConfig({
-  plugins: [tailwindcss()],
-  // تأكدي أن هذا هو اسم المستودع بالضبط على GitHub
-  base: '/my-portfolio/', 
-})
+npm install gh-pages@latest

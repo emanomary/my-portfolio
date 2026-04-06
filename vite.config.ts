f5957d4d3618dd2b80ca703e8ed64c9e1f31aa7b
emanomary <emankullab@gmail.com>
2026-04-06 13:00:31 +0300

    update 16

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({ 
     plugins: [tailwindcss()], 
-    base: '/my-portfolio/', 
+    base: '/my-portfolio/',
     build: {
     outDir: 'dist',
     assetsDir: 'assets'
